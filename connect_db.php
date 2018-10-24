4b6a0aada2ca8d81d504adb5799db2e4a410775d
NorlanXD <madrigalnorlan8@gmail.com>
2018-10-23 20:26:55 -0600

    Se agrego el backup de la base de datos

--- connect_db.php
+++ connect_db.php
@@ -1,6 +1,6 @@
 <?php
 //session_start();
-$_SESSION['connect']= new MySQLi("localhost", "root","", "e-shopdb");
+$_SESSION['connect']= new MySQLi("localhost", "root","", "rallyunodb");
 if ($_SESSION['connect'] -> connect_errno) {
 	die( "Fallo la conexión a MySQL: (" . $_SESSION['connect'] -> mysqli_connect_errno() 
 		. ") " . $_SESSION['connect'] -> mysqli_connect_error());
